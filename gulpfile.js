fbfb2052c019f1b192addaeff5f3779755e6ae59
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-06-11 16:19:56 +0300

    Deleted webp gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var server = require("browser-sync").create();
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
@@ -63,12 +62,6 @@ gulp.task("images", function () {
     ]))
     .pipe(gulp.dest("source/img"));
 });
-
-// gulp.task("webp", function () {
-//   return gulp.src("source/img/**/*.{png,jpg}")
-//     .pipe(webp({quality: 90}))
-//     .pipe(gulp.dest("source/img"));
-// });
 
 gulp.task("sprite", function () {
   return gulp.src("source/img/icon-*.svg")
